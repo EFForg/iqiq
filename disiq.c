63b8583b3c7b71400020dc4ddaf693425f54b42d
Jered Wierzbicki <jeredw@gmail.com>
2011-12-17 18:59:46 -0800

    still missing something

--- disiq.c
+++ disiq.c
@@ -1,9 +1,11 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
     FILE *fp;
+    int n = 0;
     if (argc != 2) {
         fprintf(stderr, "Usage: disiq FILE.pro\n");
         return 1;
@@ -14,20 +16,27 @@ int main(int argc, char **argv)
     }
     while (!feof(fp)) {
         int c = fgetc(fp);
+        n++;
         if (c == EOF) {
             break;
-        } else if (c == 0) {
+        }
+        if (n < 12) {  // header
+            printf("%02x ", c);
+            continue;
+        } else if (n == 12) {
+            putchar('\n');
+        }
+        if (c == 0) {
             putchar('\n');
         } else if (c == 3) {
             printf("   :    ");
         } else if (isprint(c)) {
             putchar(c);
-        } else if (c == 0x0a) {
-            //printf("\n\t");
         } else {
             printf("%02x ", c);
         }
     }
     putchar('\n');
     fclose(fp);
+    return 0;
 }
